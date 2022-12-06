7b58b0cd3a92ea361d1826ffd128752a9d7b939c
Merischessoff <gonkaschessoff@gmail.com>
2022-12-06 20:42:17 -0300

    final

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,5 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:8080/')
-    /* ==== Generated with Cypress Studio ==== */
-    cy.get('[onclick="background()"]').click();
-    cy.get('#fname').click();
-    cy.get('#lname').click();
-    cy.get('#name').click();
-    /* ==== End Cypress Studio ==== */
   })
 })
